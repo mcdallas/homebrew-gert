72db1d957715b320cdc4fa0447cb869b55dcbb67
Mike Dallas <mcdallas@protonmail.com>
2023-11-04 22:06:36 +0000

    v0.2.5

--- gert.rb
+++ gert.rb
@@ -1,8 +1,8 @@
 class Gert < Formula
   desc "Command-line tool to download media from Reddit"
   homepage "https://github.com/mcdallas/gert"
-  url "https://github.com/mcdallas/gert/releases/download/v0.2.4/gert_v0.2.4_x86_64-apple-darwin.tar.gz"
-  sha256 "e6b6ac506780ef66e7b1a1e102d3dffc4eb8710c25b442829a3b0d45330cbc64"
+  url "https://github.com/mcdallas/gert/releases/download/v0.2.5/gert_v0.2.5_x86_64-apple-darwin.tar.gz"
+  sha256 "005f9d157aa98cc6e7838bb85095df78d12f88d32bf2647352f5b6e1af21079c"
   license "MIT"
 
   depends_on "ffmpeg"
